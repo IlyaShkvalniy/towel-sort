bd5499c601a55bd18fcfb5c669372642eadcc908
IlyaShkvalniy <96240610+IlyaShkvalniy@users.noreply.github.com>
2022-02-08 17:55:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 module.exports = function towelSort (matrix) { return arguments.length > 0 ? matrix.reduce( (acc, cur, i ) =>{return acc.concat( i % 2 === 0 ? cur : cur.reverse()) }, [] ) : [] }
 
-
-// // ==============================читабельный вариант =========================================
-// module.exports = function towelSort (matrix) {
-//   let arr = arguments.length > 0 ? matrix : []
-//   arr = arr.reduce( (acc, cur, i ) =>{return acc.concat( i % 2 === 0 ? cur : cur.reverse()) }, [] )
-//   return arr
-// }
